a59559787c707fbf666ef610cab16d63b5f8c628
manghidev <31641216+manghidev@users.noreply.github.com>
2026-03-07 21:11:14 -0600

    fix: improve getDictionary function to handle missing locales gracefully

--- src/utils/i18n/dictionary.ts
+++ src/utils/i18n/dictionary.ts
@@ -3,9 +3,14 @@ import 'server-only';
 import { SupportedLanguages } from '@/utils/i18n';
 import { I18N } from '@/utils/interface/i18n';
 
+const defaultLocale = SupportedLanguages[1].code; // en-US
+
 const dynamicDictionaries = SupportedLanguages.reduce((acc, { code }) => {
     acc[code] = () => import(`@/utils/i18n/${code}.json`).then(module => module.default);
     return acc;
 }, {} as { [key: string]: () => Promise<I18N> });
 
-export const getDictionary = async (locale: string) => dynamicDictionaries[locale]();
+export const getDictionary = async (locale: string) => {
+    const loader = dynamicDictionaries[locale] ?? dynamicDictionaries[defaultLocale];
+    return loader();
+};
